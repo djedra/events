c5ad1e26cac6a6aa31e5492312205f6040be0c56
Anton Ryabenko <djedra@yandex.ru>
2025-08-16 18:37:16 +0700

    Updates

--- main.js
+++ main.js
@@ -1,69 +1,9 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ 431:
-/***/ (() => {
-
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
 /******/ 		__webpack_require__.p = "";
@@ -71,9 +11,6 @@
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-(() => {
-"use strict";
 
 ;// CONCATENATED MODULE: ./src/img/goblin.png
 const goblin_namespaceObject = __webpack_require__.p + "5c8f73a2ac04a9115cc9.png";
@@ -101,9 +38,21 @@ class Scoreboard {
   }
 }
 /* harmony default export */ const js_Scoreboard = (Scoreboard);
-// EXTERNAL MODULE: ./src/js/Cursor.js
-var Cursor = __webpack_require__(431);
-var Cursor_default = /*#__PURE__*/__webpack_require__.n(Cursor);
+;// CONCATENATED MODULE: ./src/js/Cursor.js
+class Cursor {
+  constructor() {
+    this.cursor = document.createElement("div");
+    this.cursor.classList.add("custom-cursor");
+    document.body.appendChild(this.cursor);
+    document.body.style.cursor = "none";
+    document.addEventListener("mousemove", e => this.moveCursor(e));
+  }
+  moveCursor(event) {
+    this.cursor.style.left = `${event.clientX}px`;
+    this.cursor.style.top = `${event.clientY}px`;
+  }
+}
+/* harmony default export */ const js_Cursor = (Cursor);
 ;// CONCATENATED MODULE: ./src/js/GoblinGame.js
 
 
@@ -118,7 +67,7 @@ class GoblinGame {
     this.currentPosition = -1;
     this.misses = 0;
     this.scoreboard = new js_Scoreboard();
-    this.cursor = new (Cursor_default())();
+    this.cursor = new js_Cursor();
   }
   init() {
     const grid = document.querySelector(".game-grid");
@@ -193,8 +142,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 ;// CONCATENATED MODULE: ./src/index.js
 
-
-})();
 
 /******/ })()
 ;
